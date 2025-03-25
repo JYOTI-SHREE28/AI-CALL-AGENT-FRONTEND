165c9ddc4de6037cf09c8ba3248b8e3a2d552a01
jyoti <jyotishree289@gmail.com>
2025-03-25 15:55:33 +0530

    added analytics

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Dashboard from './components/analytics-dashboard';
 import MultiCallDashboard from './components/MultiCallDashboard';
+import { Analytics } from '@vercel/analytics/next';
 
 const App = () => {
   return (
     <Router>
+      <Analytics /> {/* Moved outside of Routes */}
       <Routes>
         <Route path="/" element={<Dashboard />} />
         <Route path="/multi-call" element={<MultiCallDashboard />} />
